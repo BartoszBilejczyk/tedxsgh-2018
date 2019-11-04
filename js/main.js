f29d425da309c765c7d5432cacf3c36767a34320
BartoszBilejczyk <bartosz.bilejczyk@10clouds.com>
2019-11-04 16:14:59 +0100

    2019

--- js/main.js
+++ js/main.js
@@ -72,4 +72,42 @@ $(window).ready(function() {
       $('.go-to-cryptopia-btn').attr("disabled", true);
     }
   });
+
+
+  // newsletter
+  $(".newsletter-page-form").validate({
+    rules: {
+      "checkbox1": {
+        required: true,
+        minlength: 1
+      },
+      "checkbox2": {
+        required: true,
+        minlength: 1
+      }
+    },
+
+    // FIX
+    // Using highlight and unhighlight options we can add the error class to the parent ul which can then be selected and styled
+    highlight: function(element, errorClass, validClass) {
+      $(element).addClass(errorClass).removeClass(validClass);
+      // Keeps the default behaviour, adding error class to element but seems to break the default radio group behaviour but the below fixes that
+      $(element).closest('ul').addClass(errorClass);
+      // add error class to ul element for checkbox groups and radio inputs
+    },
+    unhighlight: function(element, errorClass, validClass) {
+      $(element).removeClass(errorClass).addClass(validClass);
+      // keeps the default behaviour removing error class from elements
+      $(element).closest('ul').removeClass(errorClass);
+      // remove error class from ul element for checkbox groups and radio inputs
+    },
+    // FIX END
+
+    errorLabelContainer: ".js-errors",
+    errorElement: "li",
+
+    messages: {
+      "checkbox[]": "Potrzebujemy wszystkich zgód, aby móc wysyłać Ci nasz newsletter."
+    }
+  });
 });
